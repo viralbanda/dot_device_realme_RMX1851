99b9a1669f426df34a207a823c2992744af16e92
viralbanda <viralbandachannel@gmail.com>
2021-03-29 22:10:04 +0000

    RMX1851: SET BOOT ANIMATION RESOLUTION

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 29
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2244
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
